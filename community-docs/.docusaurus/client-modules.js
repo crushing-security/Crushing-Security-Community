104c404fab8a9406d1caf0c10022906b17d02f10
steve-emerson-github <steve@cybsersecuritysteve.com>
2023-10-09 00:30:23 +0100

    Remove .docusaurus and node_modules directories from version control

--- community-docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/steve/development/csec_internal/apps/Crushing-Security-Community-1/community-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/steve/development/csec_internal/apps/Crushing-Security-Community-1/community-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/steve/development/csec_internal/apps/Crushing-Security-Community-1/community-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/steve/development/csec_internal/apps/Crushing-Security-Community-1/community-docs/src/css/custom.css'),
-];
